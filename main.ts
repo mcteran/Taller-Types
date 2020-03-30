c8ce57d389609f2f47f2a012a097133b0a8fac72
Camila <mc.teran@uniandes.edu.co>
2020-03-30 02:07:47 -0500

    Taller terminado

--- main.ts
+++ main.ts
@@ -2,6 +2,13 @@
 import { dataCourses } from './dataCourses.js';
 import { Course } from './course.js';
 const coursesTbody: HTMLElement = document.getElementById('courses')!; // Nodo tbody que tiene el id="courses"
+const btnfilterByName: HTMLElement = document.getElementById("button-filterByName")!;
+const inputSearchBox: HTMLInputElement = <HTMLInputElement> document.getElementById("search-box")!;
+const totalCreditElm: HTMLElement = document.getElementById("total-credits")!;
+
+btnfilterByName.onclick = () => applyFilterByName();
+
+totalCreditElm.innerHTML = `${getTotalCredits(dataCourses)}`
 
 function renderCoursesInTable(courses: Course[]): void {
   courses.forEach(c => {
@@ -11,7 +18,7 @@ function renderCoursesInTable(courses: Course[]): void {
                            <td>${c.credits}</td>`;
     coursesTbody.appendChild(trElement);
   });
-
+}
   
 function getTotalCredits(courses: Course[]): number
  {
@@ -19,4 +26,28 @@ function getTotalCredits(courses: Course[]): number
     courses.forEach((course) => totalCredits = totalCredits + course.credits);
     return totalCredits;
   }
-}
+
+  function applyFilterByName() { 
+    let text = inputSearchBox.value;
+    text = (text == null) ? '' : text;
+    clearCoursesInTable();
+    let coursesFiltered: Course[] = searchCourseByName(text, dataCourses);
+    renderCoursesInTable(coursesFiltered);
+  }
+
+  function clearCoursesInTable() {
+    while (coursesTbody.hasChildNodes()) {
+      if (coursesTbody.firstChild != null) {
+        coursesTbody.removeChild(coursesTbody.firstChild);
+       
+      }
+    }
+  }
+
+  function searchCourseByName(nameKey: string, courses: Course[]) {
+    return nameKey === '' ? dataCourses : courses.filter( c => 
+      c.name.match(nameKey));
+  }
+
+  renderCoursesInTable(dataCourses)
+  getTotalCredits(dataCourses)
